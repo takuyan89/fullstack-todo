dc68839a8a4c49548355758f63b79ded0179bc5c
Takuya <git.takuya89@gmail.com>
2024-08-07 14:01:00 +0200

    バックエンド側

---
+++ server/prisma/migrations/20240807102719_init/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `completed` on the `Todo` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Todo" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "isCompleted" BOOLEAN NOT NULL DEFAULT false
+);
+INSERT INTO "new_Todo" ("id", "title") SELECT "id", "title" FROM "Todo";
+DROP TABLE "Todo";
+ALTER TABLE "new_Todo" RENAME TO "Todo";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
