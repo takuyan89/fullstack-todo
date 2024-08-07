10883b38249676f67febf5e135ca35332f0e4556
Takuya <git.takuya89@gmail.com>
2024-08-07 16:32:50 +0200

    フロント側制作

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import type { Express, Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import cros from "cors";
 
 const app: Express = express();
 const PORT = 8080;
 
 app.use(express.json());
+app.use(cros());
 const prisma = new PrismaClient();
 
 app.get("/allTodos", async (req: Request, res: Response) => {
